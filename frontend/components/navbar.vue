9595cd9c1caa97ccafa0dd5b623bee22fba2fc22
TusharW4ni <reachtusharwani@gmail.com>
2024-11-16 18:45:28 -0600

    started homepage

---
+++ frontend/components/navbar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { UserCircleIcon } from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+  <div
+    class="relative text-white w-screen p-2 bg-gray-800 border border-b-yellow-300 border-l-transparent border-r-transparent border-t-transparent border-b-transparent"
+  >
+    <div class="grid grid-cols-3 relative z-10">
+      <div>
+        <h1 class="text-2xl font-bold uppercase space-x-1 text-yellow-200">
+          <span>P</span><span>I</span><span>N</span><span>E</span><span>A</span
+          ><span>P</span><span>P</span><span>L</span><span>E</span>
+        </h1>
+      </div>
+      <div></div>
+      <div class="flex justify-end">
+        <button class="bg-yellow-500 text-white px-4 py-2 rounded">
+          <UserCircleIcon class="h-6 w-6" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,13 @@
 <script setup>
-const res = await useFetch("http://localhost:8080/api/ping");
-console.log("res", res);
+// const token = useCookie("jwt");
+// if (token) {
+//   navigateTo("/login");
+// }
 </script>
 
 <template>
-  <div class="flex justify-center items-center w-screen h-screen">
-    <div class="underline font-mono text-4xl text-red-900">
-      {{ res.data.value }}
-    </div>
-    <nuxt-link to="/login" class="bg-red-400 p-5 rounded-xl text-2xl shadow-lg text-white hover:bg-red-600">
-      <button>register</button>
-    </nuxt-link>
+  <div class="flex flex-col w-screen h-screen bg-gray-900 text-white">
+    <navbar />
+    adsfadf
   </div>
 </template>
