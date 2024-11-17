3d1a6737a31a58dcf8526d154028c94a12dfee68
Akshay Nagarajan <akshay.h.nagarajan@gmail.com>
2024-11-17 04:38:41 -0600

    refactor navbar component for improved button styling and navigation options

--- frontend/components/navbar.vue
+++ frontend/components/navbar.vue
@@ -9,35 +9,33 @@ if (!token.value) {
 const nav = (path) => {
   navigateTo(path);
 };
+
 </script>
 
 <template>
   <div
-    class="relative text-white w-screen p-2 bg-gray-800 border border-b-yellow-300 border-l-transparent border-r-transparent border-t-transparent border-b-transparent"
-  >
+    class="relative text-white w-screen p-2 bg-gray-800 border border-b-yellow-300 border-l-transparent border-r-transparent border-t-transparent border-b-transparent">
     <div class="grid grid-cols-3 relative z-10">
       <div>
         <h1 class="text-2xl font-bold uppercase space-x-1 text-yellow-200">
-          <span>P</span><span>I</span><span>N</span><span>E</span><span>A</span
-          ><span>P</span><span>P</span><span>L</span><span>E</span>
+          <span>P</span><span>I</span><span>N</span><span>E</span><span>A</span><span>P</span><span>P</span><span>L</span><span>E</span>
         </h1>
       </div>
       <div></div>
       <div class="flex justify-end">
-        <!-- <button v-if="loggedIn" class="bg-yellow-500 text-white px-4 py-2 rounded" @click="logout">
-          <UserCircleIcon class="h-6 w-6" />
-        </button> -->
-        <!-- <button
-          class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded"
-          @click="logout"
-        >
-          <ArrowLeftStartOnRectangleIcon class="h-6 w-6" />
-        </button> -->
-        <button
-          @click="nav('/profile')"
-          v-if="loggedIn"
-          class="bg-yellow-300 rounded-full p-2 text-black"
-        >
+        <button class="bg-yellow-300 hover:bg-yellow-600 text-black px-4 mx-4 py-2 rounded" @click="nav('/home')"
+          v-if="loggedIn">
+          Home
+        </button>
+        <button class="bg-yellow-300 hover:bg-yellow-600 text-black px-4 mx-4 py-2 rounded" @click="nav('/tracking')"
+          v-if="loggedIn">
+          Tracking
+        </button>
+        <button class="bg-yellow-300 hover:bg-yellow-600 text-black px-4 mx-4 py-2 rounded" @click="nav('/report')"
+          v-if="loggedIn">
+          Reports
+        </button>
+        <button @click="nav('/profile')" v-if="loggedIn" class="bg-yellow-300 rounded-full p-2 text-black">
           <UserCircleIcon class="h-6 w-6" />
         </button>
       </div>
