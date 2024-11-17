9cc318128bd256f1b0dfc1bb8c75e28a07656191
Akshay Nagarajan <akshay.h.nagarajan@gmail.com>
2024-11-17 00:45:52 -0600

    logout functionality

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,13 @@
 <script setup>
+import { ref } from 'vue';
+import { useAuth, setLoggedIn } from '~/composables/useAuth';
+
+const { loggedIn } = useAuth();
+
 const login = async () => {
+  setLoggedIn(true);
+  // loggedIn.value = true;
+  // console.log(loggedIn);
   navigateTo("http://localhost:8080/api/auth/google/sso", {
     external: true,
   });
@@ -10,10 +18,7 @@ const login = async () => {
   <div class="flex flex-col w-screen h-screen bg-gray-900 text-white">
     <navbar />
     <div class="flex h-full justify-center items-center">
-      <button
-        class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded"
-        @click="login"
-      >
+      <button class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded" @click="login">
         Login
       </button>
     </div>
